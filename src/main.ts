a3e898fcbe5d8886b254888b40614d1e216b9a89
Pedro Henrique Duarte de Fatima <128193992+Pedrohdfatima@users.noreply.github.com>
2025-05-28 19:33:38 -0300

    Delete src directory

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { LoginComponent } from './app/components/login/login.component'; // ajuste o caminho se necessário
-import { provideHttpClient } from '@angular/common/http'; // opcional, mas comum
-
-bootstrapApplication(LoginComponent, {
-  providers: [
-    provideHttpClient(), // ou outros providers que você usa
-  ]
-});
