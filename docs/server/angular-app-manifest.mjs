07505cd93a296eba3fa6530857763723da667abf
Pedro H <ti@ti>
2025-05-28 16:44:00 -0300

    Pagina

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 936, hash: 'e79fa77c29e53b22f7c56091545f637e90626ac73d06663e7a274040670147c9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1010, hash: '1f50363ac332603ce7b73564b992f723e7861fed22ce909fff4994d782336e85', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 7779, hash: 'ca7cd221e2866cbe969fa4935788122cdf587e621c74e6dd9325721eb51952c1', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 936, hash: 'ba9288d044eda158e86a08ef677622f0051095b425bb4cc5c7596968df3be56d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1010, hash: '3a43231d2d7fc7161144cd91a1cc2de42363978348899c91319267287ad7d1a7', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 7779, hash: '6cf3595104c6571636f1ec6094348a15430a5b451010f3f533263e73f2c402f9', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-4BGBLKJW.css': {size: 2275, hash: 'vzh5r3R5tVs', text: () => import('./assets-chunks/styles-4BGBLKJW_css.mjs').then(m => m.default)}
   },
 };
--- docs/server/assets-chunks/index_csr_html.mjs
+++ docs/server/assets-chunks/index_csr_html.mjs
@@ -9,6 +9,6 @@ export default `<!doctype html>
 <style>html,body{height:100%;margin:0;padding:0}body{font-family:Arial,sans-serif;background-image:url(/src/app/components/img/fundo_log.jpeg);background-size:100%;background-position:center;background-repeat:no-repeat;background-attachment:fixed;display:flex;justify-content:center;align-items:center;text-align:center}</style><link rel="stylesheet" href="styles-4BGBLKJW.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles-4BGBLKJW.css"></noscript></head>
 <body ngcm="">
   <app-root></app-root>
-<script src="polyfills-B6TNHZQ6.js" type="module"></script><script src="main-7RM6IHWA.js" type="module"></script></body>
+<script src="polyfills-B6TNHZQ6.js" type="module"></script><script src="main-CKRD6TNE.js" type="module"></script></body>
 </html>
 `;
