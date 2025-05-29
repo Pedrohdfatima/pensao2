18fcf1fb895f60c044ec2cbfafc2b561337098a4
Pedro H <ti@ti>
2025-05-29 15:15:16 -0300

    funcional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,10 @@ import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'pensao';
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,  // <<< Isso é obrigatório para poder importar em outro standalone component
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  // sua lógica aqui
 }
